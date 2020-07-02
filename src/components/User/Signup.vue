0674e435305b4aeff7e9fb6759ce12d60f5062fe
John Duffie <john.duffie@xyleminc.com>
2020-07-02 15:02:56 -0400

    integrate firebase into main, create signup page

--- src/components/User/Signup.vue
+++ src/components/User/Signup.vue
@@ -1,13 +1,84 @@
 <template>
-    <div>
-        <p> Sign up</p>
-    </div>
-
+    <v-container>
+        <v-layout row>
+            <v-flex offset-sm3 sm6 xs12>
+                <v-card>
+                    <v-card-text>
+                        <v-container>
+                            <form>
+                                <v-layout row>
+                                    <v-flex xs12>
+                                        <v-text-field
+                                         name="email"
+                                         label="Mail"
+                                         id="email"
+                                         v-model="email"
+                                         type="email"
+                                         autocomplete="email"
+                                         required></v-text-field>
+                                    </v-flex>
+                                </v-layout>
+                                <v-layout row>
+                                    <v-flex xs12>
+                                        <v-text-field
+                                                name="password"
+                                                label="Password"
+                                                id="password"
+                                                v-model="password"
+                                                type="password"
+                                                autocomplete="new-password"
+                                                required></v-text-field>
+                                    </v-flex>
+                                </v-layout>
+                                <v-layout row>
+                                    <v-flex xs12>
+                                        <v-text-field
+                                                name="confirmPassword"
+                                                label="Confirm Password"
+                                                id="confirmPassword"
+                                                v-model="confirmPassword"
+                                                autocomplete="new-password"
+                                                type="password"
+                                                v-bind:rules="[comparePasswords]"
+                                                ></v-text-field>
+                                    </v-flex>
+                                </v-layout>
+                                <v-layout row>
+                                    <v-flex xs1>
+                                        <v-btn type="submit">Signup</v-btn>
+                                    </v-flex>
+                                </v-layout>
+                            </form>
+                        </v-container>
+                    </v-card-text>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
 
 <script>
+
     export default {
-        name: "Signup"
+        name: "Signup",
+        data() {
+            return {
+                email: '',
+                password: '',
+                confirmPassword: ''
+            }
+        },
+        computed: {
+            comparePasswords() {
+                return this.password !== this.confirmPassword ? 'Passwords do not match' : ''
+            }
+        },
+        methods: {
+            onSignup() {
+                console.log({email: this.email, password: this.password, confirmPassword:this.confirmPassword})
+                // Vuex
+            }
+        }
     }
 </script>
 
--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,28 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
 
+import * as firebase from 'firebase';
+
 Vue.config.productionTip = false
+
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyARc8UGrtqVG4VuAJ-Zq6pBtca-ZJzLWQE',
+  authDomain: 'fir-ui-base.firebaseapp.com',
+  databaseURL: 'https://fir-ui-base.firebaseio.com',
+  projectId: 'fir-ui-base',
+  storageBucket: 'fir-ui-base.appspot.com',
+  messagingSenderId: '392248120351',
+  appId: '1:392248120351:web:c5e9809b0c1682da05712a',
+  measurementId: 'G-YW98CP8HCK'
+};
 
 new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
+  render: h => h(App),
+  created: function () {
+    firebase.initializeApp(firebaseConfig);
+  }
 }).$mount('#app')
